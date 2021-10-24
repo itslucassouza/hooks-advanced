f2c4ce16b5ca2e0d597c6c0cf9acca62af54eac3
itslucassouza <lucasvini193@hotmail.com>
2021-10-24 19:11:12 -0300

    [add/hook import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';]

---
+++ src/components/useLayoutEffect/index.js
@@ -0,0 +1,30 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const Home = () => {
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const divRef = useRef();
+
+  useLayoutEffect(() => {
+    const now = Date.now();
+    while (Date.now() < now + 1500);
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
+
+  const handleClick = () => {
+    setCounted((c) => [...c, +c.slice(-1) + 1]);
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
+      <div
+        ref={divRef}
+        style={{ height: "100px", width: "100px", overflowY: "scroll" }}
+      >
+        {counted.map((c) => {
+          return <p key={`c-${c}`}>{c}</p>;
+        })}
+      </div>
+    </>
+  );
+};
